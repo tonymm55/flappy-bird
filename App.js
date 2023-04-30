607c9cd8c2e1af7a2432f6d5e477f7d964f0a416
TonyMM55 <tonymmoran@gmail.com>
2023-04-30 20:03:08 +0100

    complete up to collision detection

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from  "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Bird from "./components/Bird"
+import Obstacles from "./components/Obstacles";
 
 export default function App() {
   const screenWidth = Dimensions.get("screen").width
   const screenHeight = Dimensions.get("screen").height
-  const birdLeft = screenWidth / 1.2
-  const [birdBottom, setBirdBottom] = useState(screenHeight/1.2)
+  const birdLeft = screenWidth / 2 
+  const [birdBottom, setBirdBottom] = useState(screenHeight/2)
   const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
-  const gravity = 9.81
+  const [obstaclesLeftTwo, setObstaclesLeftTwo] = useState(screenWidth + screenWidth/2 + 10)
+  const [obstaclesNegHeight, setObstaclesNegHeight] = useState(0)
+  const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo] = useState(0)
+  const obstacleWidth = 60
+  const obstacleHeight = 300
+  const gap = 200
+  const gravity = 3.5
   let gameTimerId 
   let obstaclesLeftTimerId
+  let obstaclesLeftTimerIdTwo
   
   //start bird falling animation
   useEffect(() => {
     if (birdBottom > 0 ) {
       gameTimerId = setInterval(() => {
         setBirdBottom(birdBottom => birdBottom - gravity)
-      }, 30)
+      }, 20)
 
       return () => {
         clearInterval(gameTimerId)
@@ -28,25 +36,82 @@ export default function App() {
   
   //start first obstacles
   useEffect(() => {
-    if (obstaclesLeft > 0) {
+    if (obstaclesLeft > -obstacleWidth) {
       obstaclesLeftTimerId = setInterval(() => {
         setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
       }, 30)
+      return () => {
+        clearInterval(obstaclesLeftTimerId)
+      }
+    } else {
+      setObstaclesLeft(screenWidth)
+      setObstaclesNegHeight( - Math.random() * 100)
     }
-
-  return () => {
-  clearInterval(obstaclesLeftTimerId)
-  } 
-  
   }, [obstaclesLeft])
+  
+  //start second obstacles
+  useEffect(() => {
+    if (obstaclesLeftTwo > -obstacleWidth) {
+      obstaclesLeftTimerIdTwo = setInterval(() => {
+        setObstaclesLeftTwo(obstaclesLeftTwo => obstaclesLeftTwo - 5)
+      }, 30)
+      return () => {
+        clearInterval(obstaclesLeftTimerIdTwo)
+      }
+    } else {
+      setObstaclesLeftTwo(screenWidth)
+      setObstaclesNegHeightTwo( - Math.random() * 100)
+    }
+  }, [obstaclesLeftTwo])
+
+  //check for collisions
+  useEffect(() => {
+    if (
+    ((birdBottom < (obstaclesNegHeight + obstacleHeight + 30) ||
+    birdBottom > (obstaclesNegHeight + obstacleHeight + gap -30)) &&
+    (obstaclesLeft > screenWidth/2 -30 && obstaclesLeft < screenWidth/2 + 30) 
+    )
+    ||
+    ((birdBottom < (obstaclesNegHeightTwo + obstacleHeight + 30) ||
+    birdBottom > (obstaclesNegHeightTwo + obstacleHeight + gap -30)) &&
+    (obstaclesLeftTwo > screenWidth/2 -30 && obstaclesLeftTwo < screenWidth/2 + 30)
+    )
+    )
+    {
+      console.log("Game Over!");
+      gameOver()
+    }
+  })
+
+  const gameOver = () => {
+    clearInterval(gameTimerId)
+    clearInterval(obstaclesLeftTimerId)
+    clearInterval(obstaclesLeftTimerIdTwo)
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Flappy Bird!</Text>
+      {/* <Text className="flappy-bird-text">Flappy Bird!</Text> */}
       <Bird 
         birdBottom={birdBottom}
         birdLeft={birdLeft}
         />
+      <Obstacles
+        color={"green"}
+        obstacleWidth={obstacleWidth}
+        obstacleHeight={obstacleHeight}
+        obstaclesLeft={obstaclesLeft}
+        randomBottom={obstaclesNegHeight}
+        gap={gap}
+      />
+      <Obstacles
+        color={"red"}
+        obstacleWidth={obstacleWidth}
+        obstacleHeight={obstacleHeight}
+        obstaclesLeft={obstaclesLeftTwo}
+        randomBottom={obstaclesNegHeightTwo}
+        gap={gap}
+      />
     </View>
   )
 }
--- components/Bird.js
+++ components/Bird.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View  } from "react-native";
+import { View } from "react-native";
 
 const Bird = ({birdBottom, birdLeft}) => {
     const birdWidth = 75
